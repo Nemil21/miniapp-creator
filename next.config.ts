ebaeb2647733669673fff9dc9cf223353599615c
nemil21 <nemilshah212005@gmail.com>
2025-11-05 18:57:22 +0530

    frame src issue

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,7 @@ const nextConfig: NextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.jsdelivr.net https://unpkg.com https://www.unpkg.com; worker-src 'self' blob: data: https://unpkg.com https://www.unpkg.com; child-src 'self' blob: data:; frame-src 'self' https://*.vercel.app https://auth.privy.io https://*.privy.io; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://unpkg.com; img-src 'self' data: https: blob:; font-src 'self' data: https://unpkg.com https://cdn.jsdelivr.net; connect-src 'self' https://cdn.jsdelivr.net https://unpkg.com https://www.unpkg.com https: wss: https://auth.privy.io https://*.privy.io wss://relay.walletconnect.org;",
+              "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.jsdelivr.net https://unpkg.com https://www.unpkg.com; worker-src 'self' blob: data: https://unpkg.com https://www.unpkg.com; child-src 'self' blob: data:; frame-src 'self' https://*.vercel.app https://*.minidev.fun https://auth.privy.io https://*.privy.io; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://unpkg.com; img-src 'self' data: https: blob:; font-src 'self' data: https://unpkg.com https://cdn.jsdelivr.net; connect-src 'self' https://cdn.jsdelivr.net https://unpkg.com https://www.unpkg.com https: wss: https://auth.privy.io https://*.privy.io wss://relay.walletconnect.org;",
           },
         ],
       },
